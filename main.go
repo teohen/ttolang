42b012e79c7c332a6eb722f8e7bd8ee3fb95393d
teteo <teodorohenriquecarvalho@gmail.com>
2024-01-26 22:33:06 -0300

    feat: creates a better REPL

--- main.go
+++ main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/user"
 
 	"github.com/teohen/ttolang/repl"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
-	fmt.Printf("Feel free to type in commands\n")
+	fmt.Printf("Essa é a linguagem tto! Fique a vontade para digitar comandos\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
