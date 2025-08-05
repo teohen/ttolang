5dc2b36903476cded9b12a5eb609b36a2fadc49a
teteo <teodorohenriquecarvalho@gmail.com>
2025-08-05 18:38:01 -0300

    fix: removes unnecessary test cenarios

--- lexer/proc_test.go
+++ lexer/proc_test.go
@@ -8,56 +8,19 @@ import (
 
 func TestProcs(t *testing.T) {
 	input := `
-	cria five <- 5;
-	cria ten <- 10;
-
 	cria add <- proc(x,y) {
 		x + y
 	};
 
 	cria result <- add(five, ten);
-	!-/*5;
-	5 < 10 > 5;
-
-	se (5 < 10) {
-		devolve vdd;
-	} senao {
-		devolve falso;
-	}
-
-	10 = 10;
-	10 != 9;
-	"foobar"
-	"foo bar"
-	[1, 2];
-
-	cria i <- 1;
-	i <- i + 1;
-
-	repete(i <- i + 1 ate i < 10) {
-		i;
-	}
-
 	{nome <- "ttolang", code <- 1, op <- proc(x) { x; }}
 	{op <- proc(x) { x + 2; }}["op"](2);
-	vdd & vdd;
-	falso | vdd;
 	`
 
 	tests := []struct {
 		typeExpected    token.TokenType
 		literalExpected string
 	}{
-		{token.LET, "cria"},
-		{token.IDENT, "five"},
-		{token.ASSIGN, "<-"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "cria"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "<-"},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
 		{token.LET, "cria"},
 		{token.IDENT, "add"},
 		{token.ASSIGN, "<-"},
@@ -83,78 +46,6 @@ func TestProcs(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
-		{token.BANG, "!"},
-		{token.MINUS, "-"},
-		{token.SLASH, "/"},
-		{token.ASTERISK, "*"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.GT, ">"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.IF, "se"},
-		{token.LPAREN, "("},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RETURN, "devolve"},
-		{token.VDD, "vdd"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.ELSE, "senao"},
-		{token.LBRACE, "{"},
-		{token.RETURN, "devolve"},
-		{token.FALSE, "falso"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.INT, "10"},
-		{token.EQ, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "10"},
-		{token.NEQ, "!="},
-		{token.INT, "9"},
-		{token.SEMICOLON, ";"},
-		{token.STRING, "foobar"},
-		{token.STRING, "foo bar"},
-		{token.LBRACKET, "["},
-		{token.INT, "1"},
-		{token.COMMA, ","},
-		{token.INT, "2"},
-		{token.RBRACKET, "]"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "cria"},
-		{token.IDENT, "i"},
-		{token.ASSIGN, "<-"},
-		{token.INT, "1"},
-		{token.SEMICOLON, ";"},
-		{token.IDENT, "i"},
-		{token.ASSIGN, "<-"},
-		{token.IDENT, "i"},
-		{token.PLUS, "+"},
-		{token.INT, "1"},
-		{token.SEMICOLON, ";"},
-		{token.LOOP, "repete"},
-		{token.LPAREN, "("},
-		{token.IDENT, "i"},
-		{token.ASSIGN, "<-"},
-		{token.IDENT, "i"},
-		{token.PLUS, "+"},
-		{token.INT, "1"},
-		{token.TO, "ate"},
-		{token.IDENT, "i"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.IDENT, "i"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
 		{token.LBRACE, "{"},
 		{token.IDENT, "nome"},
 		{token.ASSIGN, "<-"},
@@ -195,14 +86,6 @@ func TestProcs(t *testing.T) {
 		{token.LPAREN, "("},
 		{token.INT, "2"},
 		{token.RPAREN, ")"},
-		{token.SEMICOLON, ";"},
-		{token.VDD, "vdd"},
-		{token.AND, "&"},
-		{token.VDD, "vdd"},
-		{token.SEMICOLON, ";"},
-		{token.FALSE, "falso"},
-		{token.OR, "|"},
-		{token.VDD, "vdd"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
